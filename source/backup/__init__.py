547e4cb84dcca31f10a0dd472aef6edab79fa936
Vladimir Kushnir <vkushnir@gmail.com>
2017-03-02 16:49:32 +0500

    Update __init__.py

--- source/backup/__init__.py
+++ source/backup/__init__.py
@@ -1,6 +1,3 @@
 ﻿__version__ = '1.2.1'
 __copyright__ = "Vladimir Kushnir aka Kvantum i(c)2017"
 
-import sys
-if sys.version_info[:2] < (2, 7):
-    raise RuntimeError('PySMI requires Python 2.7 or later')
